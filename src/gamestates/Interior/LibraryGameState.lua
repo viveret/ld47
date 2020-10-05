7822e0a6f99d6dd3470a45238855b7fb39245070
kristinalustig <kristina.lustig@gmail.com>
2020-10-05 11:05:37 -0400

    un-change physics things

--- src/gamestates/Interior/LibraryGameState.lua
+++ src/gamestates/Interior/LibraryGameState.lua
@@ -7,6 +7,37 @@ M.__index = M
 function M.new(gamestate)
     local self = setmetatable(IndoorsGameState.new(gamestate, 'Library', gamestate.images.places.library), M)
 	
+    self:addWorldBounds({
+        { -- Left
+            x = 15, y = 8,
+            w = 4, h = 80
+        },
+        { -- Top
+            x = 0, y = 20,
+            w = 100, h = 4
+        },
+        { -- Right
+            x = 85, y = 0,
+            w = 4, h = 80
+        },
+        { -- Chairs Right
+            x = 55, y = 0,
+            w = 20, h = 150
+        },
+        { -- Chairs Bottom Right
+            x = 60, y = 60,
+            w = 20, h = 13
+        },
+        { -- file cabinet
+            x = 62, y = 24,
+            w = 22, h = 5
+        },
+        { -- table
+            x = 26, y = 36,
+            w = 23, h = 1
+        }
+    })
+
     self.warps = {
         { -- Main door
             x = 45, y = 140,
