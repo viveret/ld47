54b27a2c743c8a7c23d5613aeadcd2a37029c44e
Viveret Steele <viveret.amant@gmail.com>
2020-10-05 08:29:55 -0700

    Library

--- src/gamestates/Interior/LibraryGameState.lua
+++ src/gamestates/Interior/LibraryGameState.lua
@@ -8,18 +8,10 @@ function M.new(gamestate)
     local self = setmetatable(IndoorsGameState.new(gamestate, 'Library', gamestate.images.places.library), M)
 	
     self:addWorldBounds({
-        -- { -- Left
-        --     x = 15, y = 8,
-        --     w = 4, h = 80
-        -- },
-        -- { -- Top
-        --     x = 0, y = 20,
-        --     w = 100, h = 4
-        -- },
-        -- { -- Right
-        --     x = 85, y = 0,
-        --     w = 4, h = 80
-        -- },
+        { -- Top
+            x = 0, y = 17,
+            w = self:getWidth(), h = 4
+        },
         { -- Room divider top
             x = 60, y = 0,
             w = 5, h = 30
@@ -29,16 +21,16 @@ function M.new(gamestate)
             w = 5, h = 30
         },
         { -- table
-            x = 21, y = 42,
-            w = 23, h = 7
+            x = 11, y = 42,
+            w = 26, h = 10
         }
     })
 
     self.warps = {
         { -- Main door
-            x = 45, y = 140,
+            x = 45, y = 70,
             w = 10, h = 10,
-            path = 'Overworld,96,97,x'
+            path = 'Overworld,131,97,x'
         }
     }
     self.renderWarps = true
@@ -59,8 +51,8 @@ function M:load()
     IndoorsGameState.load(self)
 
     self.indoorObjects = {
-        ropes = StaticObject.new(self.world, 50, 32, self.gamestate.images.decor.ropes),
-        tome = StaticObject.new(self.world, 70, 34, self.gamestate.images.decor.tome)
+        ropes = StaticObject.new(self.world, 63, 39, self.gamestate.images.decor.ropes),
+        tome = StaticObject.new(self.world, 80, 40, self.gamestate.images.decor.tome)
     }
 end
 
--- src/world/StaticObject.lua
+++ src/world/StaticObject.lua
@@ -10,6 +10,8 @@ function M.new(world, x, y, img)
     }, M)
 
     local w, h = img:getDimensions()
+    self.w = w
+    self.h = h
 
     self.body = lp.newBody(world, x, y, "static")
     self.shape = lp.newRectangleShape(w / 8, h / 8)
@@ -19,7 +21,15 @@ function M.new(world, x, y, img)
 end
 
 function M:draw()
+    lg.push()
+    lg.translate(-self.w / 16, -self.h / 16)
     lg.draw(self.image, self.body:getX(), self.body:getY(), 0, 1 / 8, 1 / 8)
+    if true then -- self.inProximity or self.canInteractWith
+        lg.setColor(0, 1, 0)
+        lg.rectangle('line', self.body:getX(), self.body:getY(), self.w / 8, self.h / 8)
+        lg.setColor(1, 1, 1)
+    end
+    lg.pop()
 end
 
 return M
